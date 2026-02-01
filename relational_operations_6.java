b4cb052a3e77ad9be08256246a926ea3d7155a22
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-02 04:14:30 +0530

    Add files via upload

---
+++ relational_operations_6.java
@@ -0,0 +1,13 @@
+public class relational_operations_6{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        // boolean result = a>b; greater than false
+        // boolean result = a<b;  less than true
+        // boolean result = a==b; equals to false 
+        boolean result = a!=b; //not equal to true 
+
+        System.out.println(result);
+
+    }
+}
