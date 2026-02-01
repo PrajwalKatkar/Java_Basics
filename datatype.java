b74bba6f568a6fba1138b3724170ab38e5fba01b
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-01 13:49:25 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,30 @@
+// Data types
+// primitive
+// 1 integer - byte,short ,int
+// 2 float
+// 3 character
+// 4 boolean
+
+class datatype{
+    public static void main(String args[]){
+        int num1 =5;
+        short sh=150;
+        long l= 240l;
+        byte b=126;
+
+        float f = 2.4f;
+        double d=2.4;
+
+        char c ='c';
+
+        boolean bl =true;
+        System.out.println(num1);
+        System.out.println(sh);
+        System.out.println(b);
+        System.out.println(bl);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(l);
+    }
+}
