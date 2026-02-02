c1b0567902d3b4f1fa171a353ee05f1d8ac1181e
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-03 00:49:40 +0530

    Add files via upload

---
+++ if_else_8.java
@@ -0,0 +1,10 @@
+public class if_else_8{
+    public static void main(String[] args) {
+        int  a = 10;
+        int  b=20;
+        if (a>b)
+        System.out.println("Nice");
+        else
+        System.out.println("Good morning");  // no need to use {} braces for one print statement
+    }
+}
