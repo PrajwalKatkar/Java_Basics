6e6f15caf91ce7bce029fb3c0afcd529f1da44a9
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-05 00:40:38 +0530

    Add files via upload

---
+++ do_while_14.java
@@ -0,0 +1,9 @@
+public class do_while_14{
+    public static void main(String[] args) {
+        int i = 6;
+        do { 
+            System.out.println("hii");
+            i++;
+        } while (i<=5);
+    }
+}
