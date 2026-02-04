04b594701dfb908df16a7d48ef6260d44ea20b70
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-05 00:40:02 +0530

    Add files via upload

---
+++ while_loop_13.java
@@ -0,0 +1,12 @@
+public class while_loop_13{
+    public static void main(String[] args) {
+        
+        int i =1;
+        while(i<=5){
+            System.out.println("Hii " + i);
+            i++;
+        }
+        
+
+    }
+}
