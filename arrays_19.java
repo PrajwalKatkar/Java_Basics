c5710774b8b9bbc035924779839e0fad4dc54a8b
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-07 01:12:36 +0530

    Add files via upload

---
+++ arrays_19.java
@@ -0,0 +1,8 @@
+public class arrays_19{
+    public static void main(String[] args) {
+        int num[] = {2,4,5,6,87};
+        for(int i =0;i<5;i++){
+            System.out.println(num[i]);
+        }
+    }
+}
