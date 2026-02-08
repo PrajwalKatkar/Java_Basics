e60a76dd93307d318e36f286411662f4770b3ce6
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-08 08:00:15 +0530

    Add files via upload

---
+++ encapsulation_25.java
@@ -0,0 +1,30 @@
+class Enp{
+    private int age;
+    private String name;
+
+    public void setage(int a){
+        age=a;
+    }
+    public int getage(){
+        return age;
+    }
+     public void setname(String n){
+        name=n;
+    }
+    public String getname(){
+        return name;
+    }
+}
+
+
+public class encapsulation_25{
+    public static void main(String[] args) {
+        Enp e = new Enp();
+        e.setage(18);
+        e.getage();
+        e.setname("Prajwal");
+        e.getname();
+
+        System.out.println("name: "+e.getname()+"    age: "+e.getage());
+    }
+}
