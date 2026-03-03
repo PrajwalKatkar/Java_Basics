d939afd6998fb662f7ee8060eef9c9479da587f2
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-03-03 21:03:04 +0530

    Add files via upload

---
+++ Command_line_argument.java
@@ -0,0 +1,10 @@
+public class Command_line_argument{
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+        System.out.println(args[0]); // runs at the runtime 
+        System.out.println(args[1]); // runs at the runtime 
+        // .class file saves the bitcode 
+    
+    }
+}
